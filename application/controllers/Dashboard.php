8865c204b203163007a8aecdd474767ac6179537
fitra <cahrizki.rf@gmail.com>
2020-08-02 09:14:27 +0700

    update dashboard
    add info box

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,8 +4,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dashboard extends CI_Controller {
     public function index()
     {
+        $data['anggota_count'] = $this->db->count_all('anggota');
+        $data['buku_count'] = $this->db->count_all('buku');
+        $data['rak_count'] = $this->db->count_all('rak');
+        $data['pengarang_count'] = $this->db->count_all('pengarang');
+        $data['penerbit_count'] = $this->db->count_all('penerbit');
+
         $this->load->view('templates/header');
-        $this->load->view('pages/dashboard/index');
+        $this->load->view('pages/dashboard/index',$data);
         $this->load->view('templates/footer');
     }
 }
--- application/views/pages/dashboard/index.php
+++ application/views/pages/dashboard/index.php
@@ -1,1 +1,67 @@
-<h3>Dashboard</h3>
+<div class="row justify-content-center">
+    <!-- buku -->
+    <div class="col-2">
+        <div class="info-box">
+            <!-- Apply any bg-* class to to the icon to color it -->
+            <span class="info-box-icon bg-red"><i class="fa fa-book"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Buku</span>
+                <span class="info-box-number"><?= $buku_count ?></span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+    </div>
+
+    <!-- anggota -->
+    <div class="col-2">
+        <div class="info-box">
+            <!-- Apply any bg-* class to to the icon to color it -->
+            <span class="info-box-icon bg-red"><i class="fa fa-users"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Anggota</span>
+                <span class="info-box-number"><?= $anggota_count ?></span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+    </div>
+
+    <!-- pengarang -->
+    <div class="col-2">
+        <div class="info-box">
+            <!-- Apply any bg-* class to to the icon to color it -->
+            <span class="info-box-icon bg-red"><i class="fa fa-edit"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Pengarang</span>
+                <span class="info-box-number"><?= $pengarang_count ?></span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+    </div>
+
+    <!-- pengarang -->
+    <div class="col-2">
+        <div class="info-box">
+            <!-- Apply any bg-* class to to the icon to color it -->
+            <span class="info-box-icon bg-red"><i class="fa fa-upload"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Penerbit</span>
+                <span class="info-box-number"><?= $penerbit_count ?></span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+    </div>
+
+    <!-- Rak -->
+    <div class="col-2">
+        <div class="info-box">
+            <!-- Apply any bg-* class to to the icon to color it -->
+            <span class="info-box-icon bg-red"><i class="fa fa-archive"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Rak</span>
+                <span class="info-box-number"><?= $rak_count ?></span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+    </div>
+
+</div>
